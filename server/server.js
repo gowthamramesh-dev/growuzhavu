c824db6090d12bfc0a2cd40e2a1a78711d97cbd7
sanjaysan-dev <sanjaysan3556@gmail.com>
2025-04-05 14:51:27 +0530

    Server Into MongoDb connect by dbConfig

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5172
 app.listen(port, () => {
-    console.log('Server Listerning On Port 5172')
+    console.log(`Server Listerning On Port ${port}`);
 }
 )
