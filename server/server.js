6bc868d32c43e9d50bca071a58899e42599a8767
sanjaysan-dev <sanjaysan3556@gmail.com>
2025-04-06 16:25:43 +0530

    DBconfig integrated

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+const port = process.env.PORT || 5172;
 
-const app = express()
-const port = process.env.PORT || 5172
 app.listen(port, () => {
-    console.log('Server Listerning On Port 5172')
-}
-)
+    console.log('Server Listerning On Port 5172');
+});
